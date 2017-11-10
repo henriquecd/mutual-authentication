472ada48636795c73b6f67aa51a88cc385e172d8
Matheus Magalhaes <matheusr@cpqd.com.br>
2017-11-10 16:08:05 -0200

    Fixes public docker repository name for component

--- travis/publish.sh
+++ travis/publish.sh
@@ -4,9 +4,9 @@ version="latest"
 if [ $TRAVIS_BRANCH != "master" ] ; then
   version=$TRAVIS_BRANCH
 fi
-tag=$TRAVIS_REPO_SLUG:$version
+tag=dojot/kerberos:$version
 
 
-docker build -t $tag -f docker/Dockerfile .
+docker build -t $tag .
 docker login -u="$USERNAME" -p="$PASSWD"
 docker push $tag
